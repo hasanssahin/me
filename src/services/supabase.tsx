601728b1c7a90e52509c4be4b5526b3195860493
Hasan Şahin <sahinhhasann@gmail.com>
2025-01-26 01:59:13 +0300

    update

--- src/services/supabase.tsx
+++ src/services/supabase.tsx
@@ -1,3 +1,3 @@
 import { createClient } from "@supabase/supabase-js"
 
-export default createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
+export default createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY)
